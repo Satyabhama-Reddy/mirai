c7bf41b8f690300c83372dab4a1a2d712071d169
KSTARK007 <kiranhombal98@gmail.com>
2020-04-01 22:06:58 +0530

    wrote a script to get logs

--- log.sh
+++ log.sh
@@ -1,15 +1,15 @@
 echo "Server Log"
-tail -n 10 ./logs/server.log
-echo ""
-echo ""
-echo "Run Log"
-tail -n 10 ./logs/loader.log
+tail -n $1 ./logs/server.log
 echo ""
 echo ""
 echo "Loader Log"
-tail -n 10 ./logs/run.log
+tail -n $1 ./logs/loader.log
 echo ""
 echo ""
-echo "victim Log"
-tail -n 10 ./logs/victim.log
+echo "Run Log"
+tail -n $1 ./logs/run.log
+echo ""
+echo ""
+echo "Victim Log"
+tail -n $1 ./logs/victim.log
 echo ""
