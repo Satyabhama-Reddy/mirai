37bc0cdc25efbd04df2ae0efc375876dcc6b39da
KSTARK007 <kiranhombal98@gmail.com>
2020-04-10 02:12:24 +0530

    removed pid value

--- main.sh
+++ main.sh
@@ -17,15 +17,17 @@ echo "" > ./logs/victim.log
 echo ""
 echo "Logs are cleaned"
 echo ""
+echo ""
 
 echo "Starting the CNC server"
 echo ""
+echo ""
 nohup python3 ./botnet/server.py &
 echo "The CNC Server is up and running"
+echo ""
+echo ""
 echo "Find the logs in <project root>/logs/server.log"
 echo ""
-echo "The PiD for the server Process"
-lsof -i:5000
 echo ""
 echo "sleeping for 3 seconds"
 sleep 5
@@ -34,26 +36,32 @@ sleep 5
 echo "Starting the loader"
 python3 ./loader/install/installer.py > logs/loader.out &
 echo "Loader booted"
+echo ""
+echo ""
 echo "Find the logs in <project root>/logs/loader.log"
-echo "sleeping for 3 seconds"
+echo ""
+echo ""
+echo "sleeping for 5 seconds"
 sleep 5
 
+echo ""
+echo ""
 echo "staring the run.sh process"
 ./botnet/run.sh > ./logs/run.log 2>&1 &
+echo ""
+echo ""
 echo "Find the logs in <project root>/logs/run.log"
-
-
+echo ""
+echo ""
 echo "Starting the victim server"
+echo ""
 echo ""
 nohup python3 ./victim/victim.py > ./logs/victim.log 2>&1 &
 echo "The victim Server is up and running"
+echo ""
 echo "Find the logs in <project root>/logs/victim.log"
 echo ""
-echo "The PiD for the server Process"
-lsof -i:10000
-echo ""
-
-sleep 3
+sleep 5
 
 sh ./log.sh 10
 
